21e9d00086a3a4deab1eb4b0c6a5d3093532838f
Alexander Koval <kovalidis@gmail.com>
2020-02-19 16:38:04 +0200

    Implement aggregation for tuples up to 10 elements

--- src/tuple.rs
+++ src/tuple.rs
@@ -2,64 +2,80 @@ use tantivy::{DocId, Result, Score, Searcher};
 
 use crate::agg::{Agg, AggSegmentContext, SegmentAgg, PreparedAgg};
 
-impl<A1, A2> Agg for (A1, A2)
-where
-    A1: Agg,
-    A2: Agg,
+macro_rules! impl_agg_for_tuple {
+    ( $( $a:ident => $n:tt ),+ ) => {
+
+impl<$($a,)*> Agg for ($($a,)*)
+where $(
+    $a: Agg,
+)*
 {
-    type Fruit = (A1::Fruit, A2::Fruit);
-    type Child = (A1::Child, A2::Child);
+    type Fruit = ($($a::Fruit,)*);
+    type Child = ($($a::Child,)*);
 
     fn prepare(&self, searcher: &Searcher) -> Result<Self::Child> {
-        Ok((
-            self.0.prepare(searcher)?,
-            self.1.prepare(searcher)?,
-        ))
+        Ok(($(
+            self.$n.prepare(searcher)?,
+        )*))
     }
 
     fn requires_scoring(&self) -> bool {
-        self.0.requires_scoring() || self.1.requires_scoring()
+        $(self.$n.requires_scoring()) || *
     }
 }
 
-impl<A1, A2> PreparedAgg for (A1, A2)
-where
-    A1: PreparedAgg,
-    A2: PreparedAgg,
+impl<$($a,)*> PreparedAgg for ($($a,)*)
+where $(
+    $a: PreparedAgg,
+)*
 {
-    type Fruit = (A1::Fruit, A2::Fruit);
-    type Child = (A1::Child, A2::Child);
+    type Fruit = ($($a::Fruit,)*);
+    type Child = ($($a::Child,)*);
 
     fn create_fruit(&self) -> Self::Fruit {
-        (self.0.create_fruit(), self.1.create_fruit())
+        ($(self.$n.create_fruit(),)*)
     }
 
     fn for_segment(&self, ctx: &AggSegmentContext) -> Result<Self::Child> {
-        Ok((
-            self.0.for_segment(ctx)?,
-            self.1.for_segment(ctx)?,
-        ))
+        Ok(($(
+            self.$n.for_segment(ctx)?,
+        )*))
     }
 
     fn merge(&self, acc: &mut Self::Fruit, fruit: &Self::Fruit) {
-        self.0.merge(&mut acc.0, &fruit.0);
-        self.1.merge(&mut acc.1, &fruit.1);
+        $(
+            self.$n.merge(&mut acc.$n, &fruit.$n);
+        )*
     }
 }
 
-impl<A1, A2> SegmentAgg for (A1, A2)
-where
-    A1: SegmentAgg,
-    A2: SegmentAgg,
+impl<$($a,)*> SegmentAgg for ($($a,)*)
+where $(
+    $a: SegmentAgg,
+)*
 {
-    type Fruit = (A1::Fruit, A2::Fruit);
+    type Fruit = ($($a::Fruit,)*);
 
     fn create_fruit(&self) -> Self::Fruit {
-        (self.0.create_fruit(), self.1.create_fruit())
+        ($(self.$n.create_fruit(),)*)
     }
 
     fn collect(&mut self, doc: DocId, score: Score, output: &mut Self::Fruit) {
-        self.0.collect(doc, score, &mut output.0);
-        self.1.collect(doc, score, &mut output.1);
+        $(
+            self.$n.collect(doc, score, &mut output.$n);
+        )*
     }
 }
+
+    };
+}
+
+impl_agg_for_tuple!(A1 => 0, A2 => 1);
+impl_agg_for_tuple!(A1 => 0, A2 => 1, A3 => 2);
+impl_agg_for_tuple!(A1 => 0, A2 => 1, A3 => 2, A4 => 3);
+impl_agg_for_tuple!(A1 => 0, A2 => 1, A3 => 2, A4 => 3, A5 => 4);
+impl_agg_for_tuple!(A1 => 0, A2 => 1, A3 => 2, A4 => 3, A5 => 4, A6 => 5);
+impl_agg_for_tuple!(A1 => 0, A2 => 1, A3 => 2, A4 => 3, A5 => 4, A6 => 5, A7 => 6);
+impl_agg_for_tuple!(A1 => 0, A2 => 1, A3 => 2, A4 => 3, A5 => 4, A6 => 5, A7 => 6, A8 => 7);
+impl_agg_for_tuple!(A1 => 0, A2 => 1, A3 => 2, A4 => 3, A5 => 4, A6 => 5, A7 => 6, A8 => 7, A9 => 8);
+impl_agg_for_tuple!(A1 => 0, A2 => 1, A3 => 2, A4 => 3, A5 => 4, A6 => 5, A7 => 6, A8 => 7, A9 => 8, A10 => 9);
