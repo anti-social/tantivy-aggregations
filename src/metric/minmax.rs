bc752f7ef0aba71107d52b5d508f7dd8f211c714
Alexander Koval <kovalidis@gmail.com>
2020-02-25 16:51:27 +0200

    Single macro to generate min and max aggregations

--- src/metric/minmax.rs
+++ src/metric/minmax.rs
@@ -8,8 +8,8 @@ use tantivy::schema::Field;
 
 use crate::agg::{Agg, PreparedAgg, SegmentAgg, AggSegmentContext};
 
-macro_rules! impl_min_agg_for_type {
-    ( $type:ty, $reader_fn:ident : $agg_fn:ident, $agg_struct:ident, $prepared_agg_struct:ident, $segment_agg_struct:ident ) => {
+macro_rules! impl_min_or_max_agg_for_type {
+    ( $cmp:ident, $type:ty, $reader_fn:ident : $agg_fn:ident, $agg_struct:ident, $prepared_agg_struct:ident, $segment_agg_struct:ident ) => {
 
 pub struct $agg_struct {
     field: Field,
@@ -56,9 +56,9 @@ impl PreparedAgg for $prepared_agg_struct {
         match fruit {
             None => return,
             Some(v) => {
-                if let Some(ref mut min_val) = acc {
-                    if v < *min_val {
-                        *min_val = v;
+                if let Some(ref mut value) = acc {
+                    if v.$cmp(value) {
+                        *value = v;
                     }
                 } else {
                     acc.replace(v);
@@ -69,9 +69,9 @@ impl PreparedAgg for $prepared_agg_struct {
 }
 
     };
-    ( SINGLE $(|$type:ty, $reader_fn:ident : $agg_fn:ident, $agg_struct:ident, $prepared_agg_struct:ident, $segment_agg_struct:ident|),+ ) => { $(
+    ( SINGLE => $cmp:ident $(|$type:ty, $reader_fn:ident : $agg_fn:ident, $agg_struct:ident, $prepared_agg_struct:ident, $segment_agg_struct:ident|),+ ) => { $(
 
-impl_min_agg_for_type!($type, $reader_fn : $agg_fn, $agg_struct, $prepared_agg_struct, $segment_agg_struct);
+impl_min_or_max_agg_for_type!($cmp, $type, $reader_fn : $agg_fn, $agg_struct, $prepared_agg_struct, $segment_agg_struct);
 
 pub struct $segment_agg_struct {
     ff_reader: FastFieldReader<$type>,
@@ -86,22 +86,22 @@ impl $segment_agg_struct {
 impl SegmentAgg for $segment_agg_struct {
     type Fruit = Option<$type>;
 
-    fn collect(&mut self, doc: DocId, _: Score, agg_value: &mut Self::Fruit) {
+    fn collect(&mut self, doc: DocId, _: Score, fruit: &mut Self::Fruit) {
         let v = self.ff_reader.get(doc);
-        if let Some(ref mut min_val) = agg_value {
-            if v < *min_val {
-                *min_val = v;
+        if let Some(ref mut value) = fruit {
+            if v.$cmp(value) {
+                *value = v;
             }
         } else {
-            agg_value.replace(v);
+            fruit.replace(v);
         }
     }
 }
 
     )* };
-    ( MULTI $(|$type:ty, $reader_fn:ident : $agg_fn:ident, $agg_struct:ident, $prepared_agg_struct:ident, $segment_agg_struct:ident|),+ ) => { $(
+    ( MULTI => $cmp:ident $(|$type:ty, $reader_fn:ident : $agg_fn:ident, $agg_struct:ident, $prepared_agg_struct:ident, $segment_agg_struct:ident|),+ ) => { $(
 
-impl_min_agg_for_type!($type, $reader_fn : $agg_fn, $agg_struct, $prepared_agg_struct, $segment_agg_struct);
+impl_min_or_max_agg_for_type!($cmp, $type, $reader_fn : $agg_fn, $agg_struct, $prepared_agg_struct, $segment_agg_struct);
 
 pub struct $segment_agg_struct {
     ff_reader: MultiValueIntFastFieldReader<$type>,
@@ -120,15 +120,15 @@ impl $segment_agg_struct {
 impl SegmentAgg for $segment_agg_struct {
     type Fruit = Option<$type>;
 
-    fn collect(&mut self, doc: DocId, _: Score, agg_value: &mut Self::Fruit) {
+    fn collect(&mut self, doc: DocId, _: Score, fruit: &mut Self::Fruit) {
         self.ff_reader.get_vals(doc, &mut self.vals);
         for &v in self.vals.iter() {
-            if let Some(ref mut min_val) = agg_value {
-                if v < *min_val {
-                    *min_val = v;
+            if let Some(ref mut value) = fruit {
+                if v.$cmp(value) {
+                    *value = v;
                 }
             } else {
-                agg_value.replace(v);
+                fruit.replace(v);
             }
         }
     }
@@ -137,160 +137,31 @@ impl SegmentAgg for $segment_agg_struct {
     )* };
 }
 
-impl_min_agg_for_type!(
-    SINGLE
+impl_min_or_max_agg_for_type!(
+    SINGLE => lt
     |u64, u64 : min_agg_u64, MinAggU64, PreparedMinAggU64, MinSegmentAggU64|,
     |i64, i64 : min_agg_i64, MinAggI64, PreparedMinAggI64, MinSegmentAggI64|,
     |f64, f64 : min_agg_f64, MinAggF64, PreparedMinAggF64, MinSegmentAggF64|,
     |DateTime, date: min_agg_date, MinAggDate, PreparedMinAggDate, MinSegmentAggDate|
 );
 
-impl_min_agg_for_type!(
-    MULTI
+impl_min_or_max_agg_for_type!(
+    MULTI => lt
     |u64, u64s : min_agg_u64s, MinAggU64s, PreparedMinAggU64s, MinSegmentAggU64s|,
     |i64, i64s : min_agg_i64s, MinAggI64s, PreparedMinAggI64s, MinSegmentAggI64s|,
     |f64, f64s : min_agg_f64s, MinAggF64s, PreparedMinAggF64s, MinSegmentAggF64s|,
-    |DateTime, dates: min_agg_dates, MinAggDates, PreparedMinAggDates, MinSegmentAggDates|
+    |DateTime, dates : min_agg_dates, MinAggDates, PreparedMinAggDates, MinSegmentAggDates|
 );
 
-macro_rules! impl_max_agg_for_type {
-    ( $type:ty, $reader_fn:ident : $agg_fn:ident, $agg_struct:ident, $prepared_agg_struct:ident, $segment_agg_struct:ident ) => {
-
-pub struct $agg_struct {
-    field: Field,
-}
-
-pub fn $agg_fn(field: Field) -> $agg_struct {
-    $agg_struct { field }
-}
-
-impl Agg for $agg_struct {
-    type Fruit = Option<$type>;
-    type Child = $prepared_agg_struct;
-
-    fn prepare(&self, _: &Searcher) -> Result<Self::Child> {
-        Ok(Self::Child {
-            field: self.field,
-        })
-    }
-
-    fn requires_scoring(&self) -> bool {
-        false
-    }
-}
-
-pub struct $prepared_agg_struct {
-    field: Field,
-}
-
-impl PreparedAgg for $prepared_agg_struct {
-    type Fruit = Option<$type>;
-    type Child = $segment_agg_struct;
-
-    fn for_segment(&self, ctx: &AggSegmentContext) -> Result<Self::Child> {
-        let ff_reader = ctx.reader.fast_fields().$reader_fn(self.field)
-            .ok_or_else(|| {
-                FastFieldNotAvailableError::new(
-                    ctx.reader.schema().get_field_entry(self.field)
-                )
-            })?;
-        Ok(Self::Child::new(ff_reader))
-    }
-
-    fn merge(&self, acc: &mut Self::Fruit, fruit: Self::Fruit) {
-        match fruit {
-            None => return,
-            Some(v) => {
-                if let Some(ref mut max_val) = acc {
-                    if v > *max_val {
-                        *max_val = v;
-                    }
-                } else {
-                    acc.replace(v);
-                }
-            }
-        }
-    }
-}
-
-    };
-    ( SINGLE $(|$type:ty, $reader_fn:ident : $agg_fn:ident, $agg_struct:ident, $prepared_agg_struct:ident, $segment_agg_struct:ident|),+ ) => { $(
-
-impl_max_agg_for_type!($type, $reader_fn : $agg_fn, $agg_struct, $prepared_agg_struct, $segment_agg_struct);
-
-pub struct $segment_agg_struct {
-    ff_reader: FastFieldReader<$type>,
-}
-
-impl $segment_agg_struct {
-    fn new(ff_reader: FastFieldReader<$type>) -> Self {
-        Self { ff_reader }
-    }
-}
-
-impl SegmentAgg for $segment_agg_struct {
-    type Fruit = Option<$type>;
-
-    fn collect(&mut self, doc: DocId, _: Score, agg_value: &mut Self::Fruit) {
-        let v = self.ff_reader.get(doc);
-        if let Some(ref mut max_val) = agg_value {
-            if v > *max_val {
-                *max_val = v;
-            }
-        } else {
-            agg_value.replace(v);
-        }
-    }
-}
-
-    )* };
-    ( MULTI $(|$type:ty, $reader_fn:ident : $agg_fn:ident, $agg_struct:ident, $prepared_agg_struct:ident, $segment_agg_struct:ident|),+ ) => { $(
-
-impl_max_agg_for_type!($type, $reader_fn : $agg_fn, $agg_struct, $prepared_agg_struct, $segment_agg_struct);
-
-pub struct $segment_agg_struct {
-    ff_reader: MultiValueIntFastFieldReader<$type>,
-    vals: Vec<$type>,
-}
-
-impl $segment_agg_struct {
-    fn new(ff_reader: MultiValueIntFastFieldReader<$type>) -> Self {
-        Self {
-            ff_reader,
-            vals: vec!(),
-        }
-    }
-}
-
-impl SegmentAgg for $segment_agg_struct {
-    type Fruit = Option<$type>;
-
-    fn collect(&mut self, doc: DocId, _: Score, agg_value: &mut Self::Fruit) {
-        self.ff_reader.get_vals(doc, &mut self.vals);
-        for &v in self.vals.iter() {
-            if let Some(ref mut max_val) = agg_value {
-                if v > *max_val {
-                    *max_val = v;
-                }
-            } else {
-                agg_value.replace(v);
-            }
-        }
-    }
-}
-
-    )* };
-}
-
-impl_max_agg_for_type!(
-    SINGLE
+impl_min_or_max_agg_for_type!(
+    SINGLE => gt
     |u64, u64 : max_agg_u64, MaxAggU64, PreparedMaxAggU64, MaxSegmentAggU64|,
     |i64, i64 : max_agg_i64, MaxAggI64, PreparedMaxAggI64, MaxSegmentAggI64|,
     |f64, f64 : max_agg_f64, MaxAggF64, PreparedMaxAggF64, MaxSegmentAggF64|,
     |DateTime, date: max_agg_date, MaxAggDate, PreparedMaxAggDate, MaxSegmentAggDate|
 );
-impl_max_agg_for_type!(
-    MULTI
+impl_min_or_max_agg_for_type!(
+    MULTI => gt
     |u64, u64s : max_agg_u64s, MaxAggU64s, PreparedMaxAggU64s, MaxSegmentAggU64s|,
     |i64, i64s : max_agg_i64s, MaxAggI64s, PreparedMaxAggI64s, MaxSegmentAggI64s|,
     |f64, f64s : max_agg_f64s, MaxAggF64s, PreparedMaxAggF64s, MaxSegmentAggF64s|,
