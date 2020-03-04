c704cf83e0f875c8432180736c6756bb684d61e2
Alexander Koval <kovalidis@gmail.com>
2020-03-04 13:07:41 +0200

    Added start parameter for histogram aggregation

--- src/bucket/histogram.rs
+++ src/bucket/histogram.rs
@@ -6,14 +6,15 @@ use tantivy::schema::Field;
 
 use crate::agg::{Agg, AggSegmentContext, PreparedAgg, SegmentAgg};
 
-type HistogramFruit<T> = HistogramAggRes<T>;
-
-pub fn histogram_agg_f64<SubAgg>(field: Field, interval: f64, sub_agg: SubAgg) -> HistogramAgg<SubAgg>
+pub fn histogram_agg_f64<SubAgg>(
+    field: Field, start: f64, interval: f64, sub_agg: SubAgg
+) -> HistogramAgg<SubAgg>
 where
     SubAgg: Agg,
 {
     HistogramAgg {
         field,
+        start,
         interval,
         sub_agg,
     }
@@ -24,6 +25,7 @@ where
     SubAgg: Agg,
 {
     field: Field,
+    start: f64,
     interval: f64,
     sub_agg: SubAgg,
 }
@@ -33,12 +35,13 @@ where
     SubAgg: Agg,
     <SubAgg as Agg>::Child: PreparedAgg,
 {
-    type Fruit = HistogramFruit<SubAgg::Fruit>;
+    type Fruit = Histogram<SubAgg::Fruit>;
     type Child = PreparedHistogramAgg<SubAgg::Child>;
 
     fn prepare(&self, searcher: &Searcher) -> Result<Self::Child> {
         Ok(Self::Child {
             field: self.field,
+            start: self.start,
             interval: self.interval,
             sub_agg: self.sub_agg.prepare(searcher)?,
         })
@@ -54,6 +57,7 @@ where
     SubAgg: PreparedAgg,
 {
     field: Field,
+    start: f64,
     interval: f64,
     sub_agg: SubAgg,
 }
@@ -62,11 +66,12 @@ impl<SubAgg> PreparedAgg for PreparedHistogramAgg<SubAgg>
 where
     SubAgg: PreparedAgg,
 {
-    type Fruit = HistogramFruit<SubAgg::Fruit>;
+    type Fruit = Histogram<SubAgg::Fruit>;
     type Child = HistogramSegmentAgg<SubAgg::Child>;
 
     fn create_fruit(&self) -> Self::Fruit {
-        HistogramFruit {
+        Histogram {
+            start: self.start,
             interval: self.interval,
             buckets: BTreeMap::new(),
         }
@@ -76,6 +81,7 @@ where
         let ff_reader = ctx.reader.fast_fields().f64(self.field).unwrap();
         Ok(Self::Child::new(
             ff_reader,
+            self.start,
             self.interval,
             self.sub_agg.for_segment(ctx)?,
         ))
@@ -97,6 +103,7 @@ where
     SubAgg: SegmentAgg,
 {
     ff_reader: FastFieldReader<f64>,
+    start: f64,
     interval: f64,
     sub_agg: SubAgg,
 }
@@ -105,9 +112,9 @@ impl<SubAgg> HistogramSegmentAgg<SubAgg>
 where
     SubAgg: SegmentAgg,
 {
-    fn new(ff_reader: FastFieldReader<f64>, interval: f64, sub_agg: SubAgg) -> Self {
+    fn new(ff_reader: FastFieldReader<f64>, start: f64, interval: f64, sub_agg: SubAgg) -> Self {
         Self {
-            ff_reader, interval, sub_agg
+            ff_reader, start, interval, sub_agg
         }
     }
 }
@@ -116,23 +123,29 @@ impl<SubAgg> SegmentAgg for HistogramSegmentAgg<SubAgg>
 where
     SubAgg: SegmentAgg,
 {
-    type Fruit = HistogramFruit<SubAgg::Fruit>;
+    type Fruit = Histogram<SubAgg::Fruit>;
 
     fn create_fruit(&self) -> Self::Fruit {
-        HistogramFruit {
+        Histogram {
+            start: self.start,
             interval: self.interval,
             buckets: BTreeMap::new(),
         }
     }
 
-    fn collect(&mut self, doc: DocId, score: Score, agg_value: &mut Self::Fruit) {
+    fn collect(&mut self, doc: DocId, score: Score, fruit: &mut Self::Fruit) {
         let k = self.ff_reader.get(doc);
         if k.is_nan() {
             return;
         }
 
-        let bucket_ord = (k / self.interval).floor() as u64;
-        let bucket = agg_value.buckets.entry(bucket_ord)
+        let n = k - self.start;
+        if n < 0.0 {
+            return;
+        }
+
+        let bucket_ord = (n / self.interval).floor() as u64;
+        let bucket = fruit.buckets.entry(bucket_ord)
             .or_insert_with(|| self.sub_agg.create_fruit());
 
         self.sub_agg.collect(doc, score, bucket);
@@ -140,12 +153,13 @@ where
 }
 
 #[derive(Default, Debug)]
-pub struct HistogramAggRes<T> {
+pub struct Histogram<T> {
+    start: f64,
     interval: f64,
     buckets: BTreeMap<u64, T>,
 }
 
-impl<T> HistogramAggRes<T> {
+impl<T> Histogram<T> {
     pub fn buckets(&self) -> Vec<(f64, Option<&T>)> {
         let mut res = vec!();
         let mut last_bucket_ord = if let Some(&bucket_ord) = self.buckets.keys().next() {
@@ -157,10 +171,10 @@ impl<T> HistogramAggRes<T> {
             let gap = bucket_ord - last_bucket_ord;
             if gap > 1 {
                 for i in 0_u64..(gap - 1) {
-                    res.push(((last_bucket_ord + i + 1) as f64 * self.interval, None));
+                    res.push(((last_bucket_ord + i + 1) as f64 * self.interval + self.start, None));
                 }
             }
-            res.push((*bucket_ord as f64 * self.interval, Some(&agg)));
+            res.push((*bucket_ord as f64 * self.interval + self.start, Some(&agg)));
             last_bucket_ord = *bucket_ord;
         }
         res
@@ -185,7 +199,7 @@ mod tests {
         let searcher = product_index.reader.searcher();
 
         let price_hist_agg = histogram_agg_f64(
-            product_index.schema.price, 10.0_f64, count_agg()
+            product_index.schema.price, 0.0, 10.0, count_agg()
         );
         let price_hist = searcher.agg_search(&AllQuery, &price_hist_agg)?;
         assert_eq!(
@@ -209,6 +223,32 @@ mod tests {
     }
 
     #[test]
+    fn test_histogram_agg_with_custom_start() -> Result<()> {
+        let mut product_index = ProductIndex::create_in_ram(3_u16)?;
+        product_index.index_test_products()?;
+
+        let searcher = product_index.reader.searcher();
+
+        let price_hist_agg = histogram_agg_f64(
+            product_index.schema.price, 35.0, 10.0, count_agg()
+        );
+        let price_hist = searcher.agg_search(&AllQuery, &price_hist_agg)?;
+        assert_eq!(
+            price_hist.buckets(),
+            vec!(
+                (45.0_f64, Some(&1_u64)),
+                (55.0_f64, None),
+                (65.0_f64, None),
+                (75.0_f64, None),
+                (85.0_f64, None),
+                (95.0_f64, Some(&1_u64)),
+            )
+        );
+
+        Ok(())
+    }
+
+    #[test]
     fn test_nested_histogram_agg() -> Result<()> {
         let mut product_index = ProductIndex::create_in_ram(3_u16)?;
         product_index.index_test_products()?;
@@ -219,7 +259,7 @@ mod tests {
             (
                 count_agg(),
                 histogram_agg_f64(
-                    product_index.schema.price, 10.0_f64, count_agg()
+                    product_index.schema.price, 0.0, 10.0, count_agg()
                 )
             )
         );
@@ -297,7 +337,6 @@ mod tests {
         Ok(())
     }
 
-
     #[test]
     fn test_filtered_histogram_agg() -> Result<()> {
         let mut product_index = ProductIndex::create_in_ram(3)?;
@@ -308,7 +347,7 @@ mod tests {
         let price_hist_agg = filter_agg(
             &price_query,
             histogram_agg_f64(
-                product_index.schema.price, 10.0_f64, count_agg()
+                product_index.schema.price, 0.0, 10.0, count_agg()
             )
         );
         let price_hist = searcher.agg_search(&AllQuery, &price_hist_agg)?;
